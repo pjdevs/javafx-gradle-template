c404e9d10d5cab01eb94bf91caa09c1743b0c718
MOREL Pierre-Jean <pimorel@enseirb-matmeca.fr>
2021-08-26 12:53:49 +0200

    Basic project structure

---
+++ src/test/java/javafxtest/AppTest.java
@@ -0,0 +1,11 @@
+package javafxtest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void blankTest() {
+        assertNotNull(3, "3 is not null");
+    }
+}
